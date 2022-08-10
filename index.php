a312d538396be71f1d6e39e9aedc98bb6c10fcb2
Nekmore <nekmorez@gmail.com>
2022-08-10 18:16:12 +0700

    Commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="styles/style.css">
-	<title>Copy Star</title>
+	<title>Auth</title>
 </head>
 <body>
 
